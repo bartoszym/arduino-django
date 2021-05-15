fc33a545d13db9f4a79045c9c98e4a968c31d50a
bartoszym <szymaniak98@gmail.com>
2021-05-15 14:16:32 +0200

    beginning of charts

--- arduinosite/data/urls.py
+++ arduinosite/data/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('temperatures/', TemperatureListView.as_view(), name='temperature-list'),
+    path('temperatures/charts', temperature_chart, name='temperature-chart'),
     path('temperatures/delete/<int:pk>/', TemperatureDeleteView.as_view(), name='temperature-delete'),
     path('humidities/', HumidityListView.as_view(), name='humidity-list'),
     path('humidities/delete/<int:pk>/', HumidityDeleteView.as_view(), name='humidity-delete'),
--- arduinosite/data/views.py
+++ arduinosite/data/views.py
@@ -1,9 +1,13 @@
+from django.db.models.functions import ExtractDay
+from django.db.models import Sum
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DeleteView
 
-import urllib.request
+from calendar import monthrange
+from collections import defaultdict
+from datetime import date, datetime
 
 from .models import Temperature, Humidity, Lightness
 
@@ -48,3 +52,22 @@ class LightnessDeleteView(DeleteView):
     model = Lightness
     template_name = 'data/data_object_delete.html'
     success_url = reverse_lazy('data:lightness-list')
+
+def temperature_chart(request):
+    chart_dict = defaultdict(lambda: 0)
+    temperatures_current_month = Temperature.objects.filter(
+        date_time__month=datetime.today().month
+        ).annotate(day=ExtractDay('date_time')).values('day', 'value')
+    month_length = monthrange(datetime.today().year, datetime.today().month)[1]
+    for i in range(1, month_length+1):
+        daily_values = temperatures_current_month.filter(day=i)
+        values_amount = daily_values.aggregate(amount=Sum('value'))['amount']
+        if values_amount:
+            average_value = values_amount/len(daily_values)
+            print(average_value)
+            
+    # for i in temperatures_current_month:
+    #     chart_dict[i['day']] = chart_dict[i['day']] + i.get('value')
+    #     # print(i.get('value'))
+    # print(temperatures_current_month)
+    # print(chart_dict)

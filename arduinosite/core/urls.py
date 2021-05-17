fd8346ef0c87a0700469072d36ab552637cafe47
bartoszym <szymaniak98@gmail.com>
2021-05-17 18:31:42 +0200

    added login and logout

--- arduinosite/core/urls.py
+++ arduinosite/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('get_humidity/', get_humidity, name='get-humidity'),
     path('get_lightness/', get_lightness, name='get-lightness'),
     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]

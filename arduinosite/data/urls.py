d24597fc0a793605c636ed930735c930675d3929
bartoszym <szymaniak98@gmail.com>
2021-05-17 12:47:01 +0200

    added charts for rest of data

--- arduinosite/data/urls.py
+++ arduinosite/data/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('temperatures/charts', temperature_chart, name='temperature-chart'),
     path('temperatures/delete/<int:pk>/', TemperatureDeleteView.as_view(), name='temperature-delete'),
     path('humidities/', HumidityListView.as_view(), name='humidity-list'),
+    path('humidities/charts', humidity_chart, name='humidity-chart'),
     path('humidities/delete/<int:pk>/', HumidityDeleteView.as_view(), name='humidity-delete'),
     path('lightnesses/', LightnessListView.as_view(), name='lightness-list'),
+    path('lightnesses/charts', lightness_chart, name='lightness-chart'),
     path('lightnesses/delete/<int:pk>/', LightnessDeleteView.as_view(), name='lightness-delete'),
 ]
--- arduinosite/data/views.py
+++ arduinosite/data/views.py
@@ -1,7 +1,6 @@
 from django.db.models.functions import ExtractDay
 from django.db.models import Sum
-from django.forms import forms
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DeleteView
@@ -54,8 +53,7 @@ class LightnessDeleteView(DeleteView):
     template_name = 'data/data_object_delete.html'
     success_url = reverse_lazy('data:lightness-list')
 
-def temperature_chart(request):
-    chosen_month = datetime.today().month
+def create_form(request, chosen_month):
     if request.method == 'POST':
         form = MonthForm(request.POST, initial={'month': chosen_month})
         if form.is_valid():
@@ -63,22 +61,66 @@ def temperature_chart(request):
     else:
         form = MonthForm(initial={'month': chosen_month})
         
+    return form
+
+def create_chart_data(queryset):
     chart_dict = {}
-    temperatures_current_month = Temperature.objects.filter(
-        date_time__month=chosen_month
-        ).annotate(day=ExtractDay('date_time')).values('day', 'value')
     month_length = monthrange(datetime.today().year, datetime.today().month)[1]
     
     for i in range(1, month_length+1):
-        daily_values = temperatures_current_month.filter(day=i)
+        daily_values = queryset.filter(day=i)
         values_amount = daily_values.aggregate(amount=Sum('value'))['amount']
         if values_amount:
             average_value = round(values_amount/len(daily_values), 2)
             chart_dict[i] = average_value
         else:
             chart_dict[i] = 0
+            
+    return chart_dict
+
+def temperature_chart(request):
+    chosen_month = datetime.today().month
+    form = create_form(request, chosen_month)
     
-    return render(request, 'data/temperature_chart.html', {
+    temperatures_current_month = Temperature.objects.filter(
+        date_time__month=chosen_month
+        ).annotate(day=ExtractDay('date_time')).values('day', 'value')
+    
+    chart_dict = create_chart_data(temperatures_current_month)
+    
+    return render(request, 'data/chart.html', {
+        'labels': list(chart_dict.keys()),
+        'data': list(chart_dict.values()),
+        'form': form,
+    })
+
+def humidity_chart(request):
+    chosen_month = datetime.today().month
+    form = create_form(request, chosen_month)
+    
+    humidities_current_month = Humidity.objects.filter(
+        date_time__month=chosen_month
+        ).annotate(day=ExtractDay('date_time')).values('day', 'value')
+    
+    chart_dict = create_chart_data(humidities_current_month)
+    
+    return render(request, 'data/chart.html', {
+        'labels': list(chart_dict.keys()),
+        'data': list(chart_dict.values()),
+        'form': form,
+    })
+    
+def lightness_chart(request):
+    chosen_month = datetime.today().month
+    form = create_form(request, chosen_month)
+    
+    lightnesses_current_month = Lightness.objects.filter(
+        date_time__month=chosen_month
+        ).annotate(day=ExtractDay('date_time')).values('day', 'value')
+    
+    chart_dict = create_chart_data(lightnesses_current_month)
+    
+    return render(request, 'data/chart.html', {
         'labels': list(chart_dict.keys()),
         'data': list(chart_dict.values()),
         'form': form,

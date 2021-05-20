d7ed60fe4c140dd11816aa8762a4df7a4e947f8f
bartoszym <szymaniak98@gmail.com>
2021-05-20 23:20:50 +0200

    added translations

--- arduinosite/data/forms.py
+++ arduinosite/data/forms.py
@@ -1,20 +1,20 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 
 class MonthForm(forms.Form):
     month_choices = (
-        (1,'January'),
-        (2,'February'),
-        (3,'March'),
-        (4,'April'),
-        (5,'May'),
-        (6,'June'),
-        (7,'July'),
-        (8,'August'),
-        (9,'September'),
-        (10,'October'),
-        (11,'November'),
-        (12,'December'),
+        (1,_('January')),
+        (2,_('February')),
+        (3,_('March')),
+        (4,_('April')),
+        (5,_('May')),
+        (6,_('June')),
+        (7,_('July')),
+        (8,_('August')),
+        (9,_('September')),
+        (10,_('October')),
+        (11,_('November')),
+        (12,_('December')),
         )
-    month = forms.ChoiceField(label='Choose month', choices=month_choices)
+    month = forms.ChoiceField(label=_('Choose month'), choices=month_choices)
     
